a7e8e22a77b5efbfcaa7e576c3f5825b3315e85f
cduarte <duarte.camd@gmail.com>
2023-01-13 18:43:32 -0300

    Add contructor to guest controller.

--- src/ar/com/camd/hotel/controller/GuestController.java
+++ src/ar/com/camd/hotel/controller/GuestController.java
@@ -6,6 +6,9 @@
 
 package ar.com.camd.hotel.controller;
 
+import ar.com.camd.hotel.service.GuestService;
+import ar.com.camd.hotel.service.GuestServiceImpl;
+
 /**
  * <code>GuestController</code>
  * Binds guest view with data.
@@ -14,5 +17,12 @@ package ar.com.camd.hotel.controller;
  * @version  1.0.0-SNAPSHOT
  */
 public class GuestController {
-
+	private GuestService guestService;
+	
+	/**
+	 * Initialize the guest service.
+	 */
+	public GuestController() {
+		this.guestService = new GuestServiceImpl();
+	}
 }
