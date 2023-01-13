08e4e7729aa8fd19712b877d8f8b483ccfc59922
cduarte <duarte.camd@gmail.com>
2023-01-13 18:40:19 -0300

    Add empty controller.

---
+++ src/ar/com/camd/hotel/controller/GuestController.java
@@ -0,0 +1,18 @@
+/**
+ * GuestController.java
+ * 
+ * @copyright 2023, Christian Ariel Modesto Duarte. All rights reserved.
+ */
+
+package ar.com.camd.hotel.controller;
+
+/**
+ * <code>GuestController</code>
+ * Binds guest view with data.
+ * 
+ * @author   Christian Ariel Modesto Duarte <duarte.camd@gmail.com>
+ * @version  1.0.0-SNAPSHOT
+ */
+public class GuestController {
+
+}
